6f67bea925d32bde76832103025fb153e8ef387a
Evgeny Zborovsky <yuv4ik@gmail.com>
2018-06-06 20:54:31 +0300

    Initial commit

---
+++ XFBindableStackLayout/Controls/BindableStackLayout.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using Xamarin.Forms;
+
+namespace XFBindableStackLayout
+{
+    public class BindableStackLayout : StackLayout
+    {
+        readonly Label header;
+
+        public BindableStackLayout()
+        {
+            header = new Label();
+            Children.Add(header);
+        }
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+        public static readonly BindableProperty ItemsSourceProperty =
+            BindableProperty.Create(nameof(ItemsSource), typeof(IEnumerable), typeof(BindableStackLayout),
+                                    propertyChanged: (bindable, oldValue, newValue) => ((BindableStackLayout)bindable).PopulateItems());
+
+        public DataTemplate ItemDataTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemDataTemplateProperty); }
+            set { SetValue(ItemDataTemplateProperty, value); }
+        }
+        public static readonly BindableProperty ItemDataTemplateProperty =
+            BindableProperty.Create(nameof(ItemDataTemplate), typeof(DataTemplate), typeof(BindableStackLayout));
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public static readonly BindableProperty TitleProperty =
+            BindableProperty.Create(nameof(Title), typeof(string), typeof(BindableStackLayout),
+                                    propertyChanged: (bindable, oldValue, newValue) => ((BindableStackLayout)bindable).PopulateHeader());
+
+        void PopulateItems()
+        {
+            if (ItemsSource == null) return;
+            foreach (var item in ItemsSource)
+            {
+                var itemTemplate = ItemDataTemplate.CreateContent() as View;
+                itemTemplate.BindingContext = item;
+                Children.Add(itemTemplate);
+            }
+        }
+
+        void PopulateHeader() => header.Text = Title;
+    }
+}
---
+++ XFBindableStackLayout/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace XFBindableStackLayout
+{
+    public sealed class MyColor
+    {
+        public string Name { get; }
+        public string HexCode { get; }
+
+        public MyColor(string name, string hexCode)
+        {
+            Name = name;
+            HexCode = hexCode;
+        }
+    }
+
+    public class MainViewModel
+    {
+        public IReadOnlyCollection<MyColor> MyColors { get; } = new List<MyColor>
+        {
+            new MyColor("Black", "#000000"),
+            new MyColor("Hot Pink", "#ff69b4"),
+            new MyColor("Red", "#ff0000"),
+            new MyColor("Sort of Green", "#7cff00")
+        };
+    }
+}
